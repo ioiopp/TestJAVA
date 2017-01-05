eb821bc8b20aa4224474f11745301812d2b0fb90
Chaoran SONG <Chaoran SONG>
2017-01-05 22:20:41 +0800

    Initial commit

---
+++ src/com/youmeek/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.youmeek.demo;
+
+/**
+ * Created by ioiop on 1/5/2017.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello world");
+    }
+}
